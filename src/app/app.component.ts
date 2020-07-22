a1d2d43b9da8ca0980d24a3b4cfb0a0ade118829
tomkotlar <t.kotlar@yahoo.com>
2020-07-22 14:10:17 +0100

    Update -KeyUp funcionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-basics';
-}
+
+  data = {
+    title: 'angular-basics'
+  }
+
+  onLogoClicked() {
+    alert('hello world') 
+  }
+
+  onKeyUp(newTitle: string){
+    this.data.title = newTitle
+  }
+ }
