a11bede6d7903e2611be91b13ea3141c4a758e7f
tomkotlar <t.kotlar@yahoo.com>
2020-07-26 15:14:47 +0100

    Update -View Children Decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
 import { COURSES } from 'src/db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -60,14 +60,19 @@ course = COURSES[0]
 // @ViewChild('cardRef2')
 // card2: CourseCardComponent
 
-@ViewChild('cardRef1', {read: ElementRef})
-card1: ElementRef
+// @ViewChild('cardRef1', {read: ElementRef})
+// card1: ElementRef
 
 // @ViewChild('container')
 // containerDiv: ElementRef;
 
-@ViewChild('courseImage')
-courseImage: ElementRef;
+// @ViewChild('courseImage')
+// courseImage: ElementRef;
+
+
+//View Children Decorator and Query List 
+@ViewChildren(CourseCardComponent)
+cards: QueryList<CourseCardComponent>
 
 constructor() {
   // console.log('card1', this.card1)
@@ -76,11 +81,27 @@ constructor() {
 ngAfterViewInit() {
   // console.log('card1', this.card1) checking when this  component is build in this lifecycle hook
   // console.log('courseImage', this.courseImage) can't templat reference the child outside the template for exampe course-card
+  // console.log('cards', this.cards)
 
+  this.cards.changes.subscribe(cards => console.log(cards)) 
+  //without the onCourseEdited() change nothing will happen because the hook is mounted after component is mounted
+}
+
+onCourseEdited() {
+  this.courses.push(
+    {
+      id: 1,
+      description: "angular core deep dive",
+      iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+      longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+      category: 'INTERMEDIATE',
+      lessonsCount: 10
+  }
+  )
 }
 
   onCourseSelected(course: Course) {
-      console.log('card1', this.card1)
+    //  console.log('card1', this.card1)
     //  console.log('card2', this.card2)
     //  console.log('containerDiv', this.containerDiv)
   }
